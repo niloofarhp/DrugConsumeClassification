ff44892372dc7fb9721794e566886df9f5da1af7
Niloofarhp <niloofar.hp@gmail.com>
2022-10-05 21:02:37 -0400

    final preprocess and pipeline

--- Train.py
+++ Train.py
@@ -1,33 +1,35 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import svm, tree
-from sklearn.model_selection import train_test_split 
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import svm, tree, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, f_classif 
+from sklearn.metrics import confusion_matrix, accuracy_score, ConfusionMatrixDisplay
 from sklearn.feature_selection import SelectFromModel
 from enum import Enum
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import VarianceThreshold
+import seaborn as sns
 class FeatureEnums(Enum):
     SelectFromModel = 1 
     SelectFromVarianceThr = 2
-    Non = 3
-
+    UnivariateFeature = 3
+    Non = 4
 class MLModelEnums(Enum):
     DecisionTree = 1 
     RandomForest = 2
     KNearestNeighbor = 3
     SVM = 4
-
 class featureSelection():
-    def __init__(self, model, datasetTrain, datasetTes, data):
+    def __init__(self, model, datasetTrain, dataTrainY, datasetTes):
         self.featureSelectType = FeatureEnums
         self.trainData = datasetTrain
         self.testData = datasetTes
         self.model = model
-        self.data = data
-        pass
+        self.trainDataY = dataTrainY
+
     def runFeatureSelection(self, featrueSelectName):
         X_new = self.trainData
         X_newT = self.testData
@@ -35,18 +37,31 @@ class featureSelection():
             X_new, X_newT = self.selectFeatureFromModel()
         if self.featureSelectType.SelectFromVarianceThr == featrueSelectName:
             X_new, X_newT = self.selectVarianceThreshold() 
-
+        if self.featureSelectType.UnivariateFeature == featrueSelectName:
+           X_new, X_newT = self.selectUnivariateFeature()    
         return X_new, X_newT
+
     def selectFeatureFromModel(self):
         modelf = SelectFromModel(self.model, prefit=True)
-        print(self.model.feature_importances_)
-        X_new = modelf.transform(self.data)
-        return X_new[:len(self.trainData)], X_new[len(self.trainData):]
+        # print(self.model.feature_importances_)
+        X_new = modelf.transform(self.trainData)
+        X_newT = modelf.transform(self.testData)
+        return X_new, X_newT
 
     def selectVarianceThreshold(self):
         sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
-        X_new = sel.fit_transform(self.data)
-        return X_new[:len(self.trainData)], X_new[len(self.trainData):]
+        X_new = sel.fit_transform(self.trainData)
+        X_newT = sel.transform(self.testData)
+        return X_new, X_newT
+        
+    def selectUnivariateFeature(self):
+        selector = SelectKBest(f_classif, k=4) 
+        selector.fit(self.trainData, self.trainDataY) 
+        scores = -np.log10(selector.pvalues_)
+        scores /= scores.max()
+        X_new = selector.fit_transform(self.trainData, self.trainDataY)
+        X_newT = selector.transform(self.testData)
+        return X_new, X_newT  
 class ML_Models():
     def __init__(self,trainX, trainY, testX, testY):
         self.modelNames = MLModelEnums #Enum('DecisionTree', 'RandomForest', 'KNearestNeighbor', 'SVM')
@@ -55,7 +70,7 @@ class ML_Models():
         self.trainY = trainY
         self.testX = testX  
         self.testY = testY 
-        pass
+
     def runModel(self, modelName):
         if(self.modelNames.DecisionTree == modelName):
             self.DecisionTreeClassifier()
@@ -93,6 +108,22 @@ class ML_Models():
         accuracyTest = accuracy_score(self.testY, Y_PredTest) 
         accuracyTrain = accuracy_score(self.trainY, self.model.predict(self.trainX))
         return accuracyTrain, accuracyTest
+    def predictModel(self):
+        predY = self.model.predict(self.testX)
+        return predY
+
+def dataPreparation(target = 13):
+    d = pd.read_csv("drug_consumption2.data",header=None)
+    df = pd.DataFrame(d.iloc[:,:])
+    dd = df.head()
+    scaler = MinMaxScaler()
+    train_test_data = df.loc[:,1:12]
+    scaler.fit(train_test_data)
+    scaled = scaler.fit_transform(train_test_data)
+    train_test_data = pd.DataFrame(scaled)
+    targetC = df.loc[:,int(target)] 
+    X_train, X_test, y_train, y_test = train_test_split(train_test_data, targetC, train_size=0.67, random_state = 0)
+    return X_train, X_test, y_train, y_test
 
 def calcLabelsInDataSet(y_dataSet):
     print((['CL0'] == y_dataSet).sum()) # 11
@@ -105,66 +136,81 @@ def calcLabelsInDataSet(y_dataSet):
     print((['NonUser'] == y_dataSet).sum()) # 167
     print((['User'] == y_dataSet).sum()) # 167
     
-
-def convertMultiClassLabelsToBinaryLabels(y_dataSet):
-    #convert multiclasses to binary classes
+def convertMultiClassLabelsToBinaryLabels(y_dataSet, biThr):
+    labelCls = ['CL0','CL1','CL2','CL3','CL4','CL5','CL6']
     for i in range(len(y_dataSet)):
         label = y_dataSet[i]
-        if label  == 'CL0' or label  == 'CL1' :
+        if labelCls.index(label) < biThr:
             y_dataSet[i] = 'NonUser'
-        if label  == 'CL6' or label  == 'CL2' or label  == 'CL3' or label  == 'CL4' or label == 'CL5':
-            y_dataSet[i] = 'User'    
-
-def ConvertMultiToBinary(y_train, y_test):
+        else:
+            y_dataSet[i] = 'User' 
+               
+def ConvertMultiToBinary(y_train, y_test, biThr):
         y_testArray = np.array(y_test)
         y_trainArray = np.array(y_train)
+        print("before binarization label numbers:")
         calcLabelsInDataSet(y_trainArray)
-        calcLabelsInDataSet(y_testArray)
-        convertMultiClassLabelsToBinaryLabels(y_trainArray)
-        convertMultiClassLabelsToBinaryLabels(y_testArray)
-        calcLabelsInDataSet(y_testArray)
+        convertMultiClassLabelsToBinaryLabels(y_trainArray, biThr)
+        convertMultiClassLabelsToBinaryLabels(y_testArray, biThr)
+        print("after binarization label numbers:")
+        calcLabelsInDataSet(y_trainArray)
         return y_trainArray, y_testArray
 
 def main():
-    featureSelectName = [FeatureEnums.SelectFromModel,FeatureEnums.SelectFromVarianceThr, FeatureEnums.Non]
-    modelNames = [MLModelEnums.DecisionTree, MLModelEnums.RandomForest, MLModelEnums.KNearestNeighbor, 
-    MLModelEnums.SVM]
-    d = pd.read_csv("drug_consumption2.data",header=None)
-    df = pd.DataFrame(d.iloc[:,:], columns = d.columns[:])
-    # print(df.columns)
-    train_test_data = df.iloc[:,:12]
-    target_data_Alcohol = df.iloc[:,31]  
-    X_train, X_test, y_train, y_test = train_test_split(train_test_data, target_data_Alcohol, train_size=0.67, random_state = 0)
-    y_trainArray, y_testArray = ConvertMultiToBinary(y_train, y_test)
-    models = ML_Models(X_train, y_trainArray, X_test, y_testArray)
-    fSName = featureSelectName[0]
-    mName = modelNames[1]
-    if(fSName == FeatureEnums.SelectFromModel and (mName == MLModelEnums.DecisionTree or mName == MLModelEnums.RandomForest)):
-        accTrain, accTest = models.runModel(mName)
-    featureSelect = featureSelection(models.model, X_train, X_test, train_test_data)
-    X_trainN, X_testN = featureSelect.runFeatureSelection(fSName)
-    accTrainN, accTestN = models.runModel(mName)
-
+    featureSelectName = [FeatureEnums.SelectFromModel,FeatureEnums.SelectFromVarianceThr, FeatureEnums.UnivariateFeature, FeatureEnums.Non]
+    modelNames = [MLModelEnums.DecisionTree, MLModelEnums.RandomForest, MLModelEnums.KNearestNeighbor, MLModelEnums.SVM]
+    drug_names = ['alcohol 5', 'Amphetamines 1', 'Crack 1' , 'Ecstasy 1', 'Nicotine 1', 'VSA 2']
+    binarizationThr = [5,1,1,1,1,2]
+    targetDrugs = ['13'    ,'14'         ,'21'   ,'22'    ,'30'     ,'31']
+    for i in range(len(targetDrugs)):
+        X_train, X_test, y_train, y_test = dataPreparation(targetDrugs[i])
+        y_trainArray, y_testArray = ConvertMultiToBinary(y_train, y_test, binarizationThr[i])
+        print("the target drug is :", drug_names[i] ,"*********************************************")
+        for mName in modelNames:
+            bestTestAc = 0
+            selectedF = ""
+            print("the model is :")
+            print(mName)
+            for fsName in featureSelectName:
+                if (mName == MLModelEnums.KNearestNeighbor or mName == MLModelEnums.SVM) and fsName == FeatureEnums.SelectFromModel:
+                    continue
+                # print("feature selection name :")
+                # print(fsName)
+                models = ML_Models(X_train, y_trainArray, X_test, y_testArray)
+                accTrain, accTest = models.runModel(mName)
+                # print("model performane before feature selection ->")
+                # print("train data :", accTrain, "test data :", accTest)
+                featureSelect = featureSelection(models.model, X_train, y_trainArray, X_test)
+                X_trainN, X_testN = featureSelect.runFeatureSelection(fsName)
+                modelsF = ML_Models(X_trainN, y_trainArray, X_testN, y_testArray)
+                accTrainN, accTestN = modelsF.runModel(mName)
+                # print("model performane after feature selection ->")
+                # print("model performane -> train data :", accTrainN, "test data :", accTestN)
+                if(bestTestAc < accTestN):
+                    bestSelectFM = featureSelect
+                    selectedF = fsName
+                    selectedW = modelsF
+                    bestTestAc = accTestN     
+            print("confusion matrix for the model selected using:", selectedF , "testACC : ", bestTestAc)
+            cfM = confusion_matrix(y_testArray, modelsF.predictModel())
+            print(cfM)
+            ConfusionMatrixDisplay(cfM, display_labels=modelsF.model.classes_).plot()
+            precision = cfM[1][1] / (cfM[1][1] + cfM[0][1])
+            recall = cfM[1][1] / (cfM[1][0] + cfM[1][1])
+            sensitivity = recall
+            specificity = cfM[0][0] / (cfM[0][0] + cfM[0][1])
+            print("presicion: ",precision)
+            print("recal: ",recall)
+            print("sensivity: ",sensitivity)
+            print("specificity: ",specificity)
+            # fpr, tpr, thresholds = metrics.roc_curve(y_testArray,selectedW.model.predict(bestSelectFM.runFeatureSelection(selectedF)[1]))
+            # roc_auc = metrics.auc(fpr, tpr)
+            metrics.RocCurveDisplay.from_estimator(selectedW.model, bestSelectFM.runFeatureSelection(selectedF)[1], y_testArray) 
+            plt.plot()
+            # metrics.plot_roc_curve(selectedW.model, bestSelectFM.runFeatureSelection(selectedF)[1], y_testArray) 
+            print("the end of model with 3 different feature selection......................")    
+        print()
     return
+
 if __name__ == '__main__':
     main() 
-
-
-
-
-# linear = svm.SVC(kernel='linear', C=1, decision_function_shape='ovo').fit(X_train, y_train)
-# linear_pred = linear.predict(X_test)
-# cm_lin = confusion_matrix(y_test, linear_pred)
-# rbf = svm.SVC(kernel='rbf', gamma=1, C=1, decision_function_shape='ovo').fit(X_train, y_train)
-# poly = svm.SVC(kernel='poly', degree=3, C=1, decision_function_shape='ovo').fit(X_train, y_train)
-# sig = svm.SVC(kernel='sigmoid', C=1, decision_function_shape='ovo').fit(X_train, y_train)
-
-
-# #Using Pearson Correlation
-# plt.figure()#figsize=(12,10))
-# cor = df.corr()
-# sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
-# plt.show()
-# cor_target = abs(cor[12])
-# relevant_features = cor_target[cor_target>0.5]
-# relevant_features    
